90c0d619f320cad3d7f90f40f8499b6a7f4574cd
cristianionion <63064438+cristianionion@users.noreply.github.com>
2023-01-07 18:22:28 -0800

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitGame()
